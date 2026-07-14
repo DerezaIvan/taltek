9ce4ea6bbdaee245dd3c3d9ef77770dfb7da7c75
Ivan Dereza <dereza2012@yandex.ru>
2026-07-14 12:33:57 +0300

    upd: внести правки по архитектуре, изменить некоторые элементы

--- src/presentation/components/sections/OperationsContacts/index.ts
+++ src/presentation/components/sections/OperationsContacts/index.ts
@@ -1,1 +1,4 @@
-export { default as OperationsContacts } from './OperationsContacts.svelte';
+export { default as OperationsContacts } from './OperationsContacts/OperationsContacts.svelte';
+export { default as DispatcherCard } from './DispatcherCard/DispatcherCard.svelte';
+export { default as PhoneContactRow } from './PhoneContactRow/PhoneContactRow.svelte';
+export { default as TerritoryCard } from './TerritoryCard/TerritoryCard.svelte';
